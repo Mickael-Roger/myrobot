acf26d66ceec44b0f5c7413e37eb392e06cb187b
Mickael Roger <mickael.roger@thalesgroup.com>
2020-10-24 14:04:56 +0200

    Camera release version

--- robot/src/camera.py
+++ robot/src/camera.py
@@ -5,53 +5,61 @@ import json
 
 import socket
 import cv2
+import time
 
 
 
 class Stream(Thread):
-    
+
     def __init__(self):
-        threading.Thread.__init__(self)
+        Thread.__init__(self)
         self.stop = 1
 
-    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('', 1884))
+
+        self.conn = None
+
+    def run(self):
+        print("Run")
         self.stop = 0
         self.cap = cv2.VideoCapture(0)
-
         
         if self.cap.isOpened():
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(('', 1884))
-                s.listen()
-                conn, addr = s.accept()
+            self.socket.listen()
+            self.conn, addr = self.socket.accept()
 
-                with conn:
-                    while self.stop == 0:
-                        try:
-                            ret, frame = self.cap.read()
-                            if ret:
-                                _, jpgframe=cv2.imencode('.jpg', frame)
-                                conn.sendall(jpgframe)
-                        except:
-                            pass
+            while self.stop == 0:
+                #self.conn.append(conn)
+                #self.socket.setblocking(False)
+                
+                #if len(self.conn) > 0:
+                #for conn in self.conn:
+                try:
+                    ret, frame = self.cap.read()
+                    if ret:
+                        _, jpgframe=cv2.imencode('.jpg', frame)
+                        self.conn.sendall(jpgframe)
+                except:
+                    pass
+                #else:
+                #    time.sleep(0.05)
 
 
     def stopStream(self):
+        #for conn in self.conn:
         self.stop = 1
+        self.conn.close()
+        self.socket.close()
+        self.cap.release()
 
-    
-
-    # Later: For each connection start a thread
 
 
 class Camera():
 
     def __init__(self):
         self.queue = msg.msg(msgName='camera')
-        self.stream = None
-        self.server = None
-                    
-
+            
 
     def dispatch(self, client, userdata, message):
 
@@ -61,21 +69,12 @@ class Camera():
             msg = json.loads(message.payload.decode('utf8'))
 
             if msg['action'] == 'startstream':
-                if self.stream == None :
-                    self.stream = Stream()
-                    print('start')
-                    self.stream.start()
-                    print('end start')
+                self.stream = Stream()
+                self.stream.start()
             
             elif msg['action'] == 'stopstream':
-                if self.stream != None :
-                    print("stop 1")
-                    self.stream.stopStream()
-                    print("stop 2")
-                    self.stream.join()
-                    print("stop 3")
-                    self.stream = None
-                    print("stop 4")
+                self.stream.stopStream()
+                del self.stream
 
         except:
             pass
