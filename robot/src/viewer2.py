31ebc6739102fa1d734e49a82abaf6ef949e93d3
Mickael Roger <mickael.roger@thalesgroup.com>
2020-10-24 19:13:21 +0200

    Add display rectangle on mouvement

--- robot/src/viewer2.py
+++ robot/src/viewer2.py
@@ -7,6 +7,7 @@ import cv2
 
 import time
 import functools
+import imutils
 
 
 
@@ -21,7 +22,7 @@ class Viewer():
 
             print("Connected", flush=True)
 
-            firstframe = 0
+            last_frame=None
 
             while True:
                 #try:
@@ -30,21 +31,42 @@ class Viewer():
                 end = bytes.find(b'\xff\xd9')
 
                 if start != -1 and end != -1:
-                    frame=bytes[start:end+2]
+                    img=bytes[start:end+2]
                     bytes=bytes[end+2:]
-                    img = cv2.imdecode(np.fromstring(frame, dtype=np.uint8),cv2.IMREAD_COLOR)
+                    frame = cv2.imdecode(np.fromstring(img, dtype=np.uint8),cv2.IMREAD_COLOR)
 
-                    if firstframe > 0:
-                        diff = cv2.absdiff(last_frame, img)
-                        
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-                        cv2.imshow('robot', diff)
-                        key = cv2.waitKey(1) & 0xFF
-                    
-                    else:
-                        firstframe = 1
-                        
-                    last_frame=img.copy()
+                    if last_frame is None:
+                        last_frame=gray
+                        continue
+
+                    frameDelta = cv2.absdiff(last_frame, gray)
+                    thresh = cv2.threshold(frameDelta, 10, 255, cv2.THRESH_BINARY)[1]
+
+                    thresh = cv2.dilate(thresh, None, iterations=2)
+
+                    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (51,51)))
+
+                    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    cnts = imutils.grab_contours(cnts)
+
+                    for c in cnts:
+                        if cv2.contourArea(c) < 500:
+                            cv2.fillPoly(thresh, pts=[c], color=0)
+                            continue
+
+                        (x, y, w, h) = cv2.boundingRect(c)
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+                    cv2.imshow('robot', frame)
+                    key = cv2.waitKey(1) & 0xFF
+
+                    last_frame=gray.copy()
+
+
                 #except:
                 #    pass
 
